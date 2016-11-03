281649e6b25b1e34ed53bcf6f42c5154243e48b6
oregilron <oregilron@gmail.com>
2016-11-03 15:29:13 +0200

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?&appid=a919a5867ec603347cb3aea9b994221c&units=metric';
+
+
+//a919a5867ec603347cb3aea9b994221c
+
+module.exports = {
+  getTemp:function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requesturl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+     return axios.get(requesturl).then(function (res){
+      if (res.data.cod && res.data.message){
+        throw new Error('No city found');
+      }else {
+        return res.data.main.temp;
+      }
+    }, function (res){
+      throw new Error('No city found');
+    });
+  }
+}
